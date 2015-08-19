83dff3525cd0cf638c7c7077990134a7fa2046c6
Fraida Fund <ffund@nyu.edu>
2015-08-19 13:40:21 -0400

    Fixed typo in resources-80

--- resource_setup/resources-80.sh
+++ resource_setup/resources-80.sh
@@ -2,8 +2,7 @@
 ISPA1="pc1.instageni.illinois.edu -p 33597"
 ISPA2="pc1.instageni.illinois.edu -p 33598"
 ISPB1="pc1.instageni.illinois.edu -p 33599"
-ISPB2="pc1.instageni.illinois.edu -p 3360"
+ISPB2="pc1.instageni.illinois.edu -p 33600"
 CA="pc1.instageni.illinois.edu -p 33594"
 CB="pc1.instageni.illinois.edu -p 33595"
 CP="pc1.instageni.illinois.edu -p 33596"
-
